688586c380ba2ab7b49d6644fb16d1b8a10f880a
Chowdary Immanni <chowdaryimmanni@gmail.com>
2024-10-17 22:13:35 +0530

    added reset each day feature

--- popup.js
+++ popup.js
@@ -1,14 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    chrome.storage.sync.get(["numberOfTimes", "timeSpent"], (data) => {
-        const { numberOfTimes = 0, timeSpent } = data;
-
-
+    chrome.storage.sync.get(["numberOfTimes"], (data) => {
+        const { numberOfTimes = 0 } = data;
         document.getElementById('visited-time').innerText = numberOfTimes;
-        // const minutes = Math.floor( timeSpent / (1000 * 60) );
-        // const seconds = Math.floor( (timeSpent % (1000 * 60) ) / 1000);
-        
-        // const formattedTime = `${String(minutes).padStart(2, '0')}` + `:` + `${String(seconds).padStart(2, '0')}`;
-
-        // document.getElementById('time-spent').innerText = formattedTime;
     });
 });
